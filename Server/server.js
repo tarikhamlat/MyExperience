668e5736c1fc2446b031ef33ed28bdcbe1fa4f2f
Tarik HAMLAT <hthamlat@gmail.com>
2019-03-24 20:10:43 +0100

    Create package file

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,10 @@
-var http = require('http');
+var express = require('express');
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Salut tout le monde !');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes à l\'accueil');
 });
-server.listen(8080);
+
+app.listen(8080);
